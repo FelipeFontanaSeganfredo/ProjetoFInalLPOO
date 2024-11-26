0c51e6c2cdeac41f035664cbb33b2998a59bb65e
FelipeFontanaSeganfredo <felipefseganfredo@gmail.com>
2024-11-26 13:24:21 -0300

    Início do projeto
    Criação inicial usando o maven

---
+++ TransportadoraAreia/src/main/java/com/mycompany/transportadoraareia/TransportadoraAreia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.transportadoraareia;
+
+/**
+ *
+ * @author User PC
+ */
+public class TransportadoraAreia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
